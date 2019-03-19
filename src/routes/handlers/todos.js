f6799a2e4ff8384054c9c8e7d969ce9716d851f5
Guilherme Sousa <outofs1gh7@gmail.com>
2019-03-19 02:53:13 +0000

    Upgrade todos request handlers

--- src/routes/handlers/todos.js
+++ src/routes/handlers/todos.js
@@ -1,17 +1,79 @@
 'use strict';
 
+import { createTask, deleteTask, updateTask } from '../../actions/todos';
+import DB from '../../db';
+import server from '../../server';
+
 const Handlers = {
 
-    fetchTasks: (request, h) => {
+    fetchTasks: async (request, h) => {
+        try {
+            const { filter, orderBy } = request.query;
+
+            const tasks = await server.methods.fetchTasks(filter, orderBy);
+
+            return h.response(tasks).code(200);
+        } catch (error) {
+            console.log(error);
+
+            return h.response().code(500);
+        }
     },
 
-    createTask: (request, h) => {
+    createTask: async (request, h) => {
+        try {
+            const newTask = createTask(request.payload.description);
+    
+            if (!newTask) {
+                return h.response().code(400);
+            }
+    
+            await DB.clear();
+    
+            return h.response(newTask).code(201);
+        } catch (error) {
+            console.log(error);
+
+            return h.response().code(500);
+        }
     },
 
-    updateTask: (request, h) => {
+    updateTask: async (request, h) => {
+        try {
+            const id = encodeURIComponent(request.params.id);
+            const updatedTask = updateTask(id);
+            
+            if (!updatedTask) {
+                return h.response().code(404);
+            }
+
+            await DB.clear();
+
+            return h.response(updatedTask).code(200);
+        } catch (error) {
+            console.log(error);
+
+            return h.response().code(500);
+        }
     },
 
-    deleteTask: (request, h) => {
+    deleteTask: async (request, h) => {
+        try {
+            const id = encodeURIComponent(request.params.id);
+            const deletedTask = deleteTask(id);
+            
+            if (!deletedTask) {
+                return h.response().code(404);
+            }
+
+            await DB.clear();
+
+            return h.response().code(200);
+        } catch (error) {
+            console.log(error);
+
+            return h.response().code(500);
+        }
     }
 
 };
